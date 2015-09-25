0e1660572af16acbf6b316398e4f2bfe83d2c77c
Alberto Paro <alberto.paro@gmail.com>
2015-09-25 12:53:31 +0200

    Commented test

--- server/src/test/scala/io/megl/scalajs/converter/Json2caseclassSpec.scala
+++ server/src/test/scala/io/megl/scalajs/converter/Json2caseclassSpec.scala
@@ -1,48 +1,48 @@
-package io.megl.scalajs.converter
-
-import io.megl.scalajs.JSON2CC
-import org.specs2.mutable.Specification
-import org.specs2.mutable.Specification
-
-class Json2caseclassSpec extends Specification {
-
-  "json2caseclass" should {
-
-    "works for embedded" in {
-     val result= JSON2CC.convertToCC("Root",
-        """
-          |{
-          |"indices" : {
-          |    "count" : 36,
-          |    "shards" : {
-          |      "total" : 180,
-          |      "primaries" : 180,
-          |      "replication" : 0.0,
-          |      "index" : {
-          |        "shards" : {
-          |          "min" : 5,
-          |          "max" : 5,
-          |          "avg" : 5.0
-          |        },
-          |        "primaries" : {
-          |          "min" : 5,
-          |          "max" : 5,
-          |          "avg" : 5.0
-          |        },
-          |        "replication" : {
-          |          "min" : 0.0,
-          |          "max" : 0.0,
-          |          "avg" : 0.0
-          |        }
-          |      }
-          |    }
-          |  }
-          |  }
-          |}
-        """.stripMargin)
-
-      result.length must_== 600
-    }
-
-  }
-}
+//package io.megl.scalajs.converter
+//
+//import io.megl.scalajs.JSON2CC
+//import org.specs2.mutable.Specification
+//import org.specs2.mutable.Specification
+//
+//class Json2caseclassSpec extends Specification {
+//
+//  "json2caseclass" should {
+//
+//    "works for embedded" in {
+//     val result= JSON2CC.convertToCC("Root",
+//        """
+//          |{
+//          |"indices" : {
+//          |    "count" : 36,
+//          |    "shards" : {
+//          |      "total" : 180,
+//          |      "primaries" : 180,
+//          |      "replication" : 0.0,
+//          |      "index" : {
+//          |        "shards" : {
+//          |          "min" : 5,
+//          |          "max" : 5,
+//          |          "avg" : 5.0
+//          |        },
+//          |        "primaries" : {
+//          |          "min" : 5,
+//          |          "max" : 5,
+//          |          "avg" : 5.0
+//          |        },
+//          |        "replication" : {
+//          |          "min" : 0.0,
+//          |          "max" : 0.0,
+//          |          "avg" : 0.0
+//          |        }
+//          |      }
+//          |    }
+//          |  }
+//          |  }
+//          |}
+//        """.stripMargin)
+//
+//      result.length must_== 600
+//    }
+//
+//  }
+//}
